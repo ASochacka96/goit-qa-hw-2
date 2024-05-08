d96ce96a5d580986a8f44e411d32282584930d62
Anna <a.sochacka96@gmail.com>
2024-05-08 13:27:59 +0200

    Test1

---
+++ cypress/e2e/test1.cy.js
@@ -0,0 +1,16 @@
+describe('Pierwszy test', () => {
+  it('Logowanie i wylogowanie ze strony LMS', () => {
+    //Polecenie otwarcia strony logowania 
+    cy.visit('https://www.edu.goit.global/account/login');
+    //Polecenie wyszukania pola email i wpisania email **user888@gmail.com**
+    cy.get('#user_email').type("user888@gmail.com");
+    //Polecneie wyszukania pola hasło i wprowadzenia hasła **1234567890**
+    cy.get('#user_password').type("1234567890");
+    //Polecenie znalezienia i wciśnięcia przycisku **Log in**
+    cy.get('.eckniwg2').click();
+    //Polecnie znalezienia przycisku w prawym górnym rogu i otworzenie menu
+    cy.get('#open-navigation-menu-mobile').click();
+    //Polecenie znalezienia i wciśnięcia przycisku **Log out**
+    cy.get(':nth-child(12) > .next-bve2vl').click();
+  });
+});
