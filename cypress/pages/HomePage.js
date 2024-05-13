d506df2cff7bb5e5229ce6564c4eecf6bb42e0f7
Anna <a.sochacka96@gmail.com>
2024-05-13 11:04:06 +0200

    Homework number 3

---
+++ cypress/pages/HomePage.js
@@ -0,0 +1,10 @@
+export class HomePage {
+    visit() {
+        cy.visit('https://www.edu.goit.global/account/login')
+    }
+
+    logout() {
+        cy.get('#open-navigation-menu-mobile').click();
+        cy.contains('Log out').click();
+    }
+}
